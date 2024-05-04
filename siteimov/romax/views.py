6161aa884557c18f8f30dc248be0d686454939ea
pamen <p.a.mendonc@gmail.com>
2024-05-04 22:48:54 +0100

    push da pagina de registo

--- siteimov/romax/views.py
+++ siteimov/romax/views.py
@@ -48,21 +48,28 @@ def criar_conta_page(request):
 def criar_conta(request):
     # TODO page para se nao foi possivel criar conta (failed server-side validation or server error (5xx))
 
+    #TODO tirar antes de delivery
+    s='#'*10 + '\n'
+    print(s*3)
+    print(request.POST)
+    print(s*3)
 
-    #Criar conta
+    #TODO server side validation
     user = User.objects.create_user(request.POST['email'],
                                     request.POST['email'],
                                     request.POST['password']
                                     )
+
+
     Cliente.objects.create(
-        user=user,
+        user= user,
         nomeCompleto = request.POST['nome-completo'],
-        telemovel = request.POST['telemovel'],
-        idade = request.POST['idade'],
-        estadoCivil = request.POST['Estado-Civil'],
-        nif = request.POST['NIF'],
+        telemovel = int(request.POST['telemovel']),
+        idade = None if 'idade' in request.POST else int(request.POST['idade']),
+        estadoCivil = None if 'idade' in request.POST else request.POST['Estado-Civil'],
+        nif = int(request.POST['NIF']),
         cc = request.POST['CC'],
-        animais = request.POST['tem-animais'],
+        animais = 'tem-animais' in request.POST,
     )
 
 
